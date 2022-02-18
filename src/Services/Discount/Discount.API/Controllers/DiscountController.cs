30014e90d82b091686a5bd0d31ff32b8a433fd1f
Anderson Masiero <anderson.masiero@gmail.com>
2022-02-17 22:03:43 -0300

    Adição Controller configuração da injeção de dependência no startup remoção de classes padrões criadas junto com projeto

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,58 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Discount.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+
+        [HttpGet("{productName}", Name="GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _repository.GetDiscount(productName);
+
+            return Ok(coupon);
+            //return coupon == null ? NotFound() : Ok(coupon);
+        }
+
+        [HttpPost(Name="CreateDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            var affected  = await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut(Name = "UpdateDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            var affected = await _repository.UpdateDiscount(coupon);
+            return Ok(affected);
+        }
+
+        [HttpDelete("{productName}", Name="DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            var affected = await _repository.DeleteDiscount(productName);
+            return Ok(affected);
+        }
+    }
+}
