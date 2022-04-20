7478aa4ca531227277966d6d0d6f68fd87d363ea
Anderson Masiero <anderson.masiero@gmail.com>
2022-04-20 20:21:36 -0300

    - Controller Consumindo DiscountGrpcClient - Adição da URL do Grpc Server no appSettings - Correção no AppSettings Developer para testes em localhost - Registro/injeção do protobuffer e do GrpcClient - Ajustes na nomenclatura do cupon de desconto Samsung

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,5 +1,7 @@
 using Basket.API.Entities.Repositories.Classes;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories.Interfaces;
+using Discount.Grpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -33,6 +35,8 @@ namespace Basket.API
             });
 
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+            services.AddScoped<DiscountGrpcService>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
--- src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -44,7 +44,7 @@ namespace Discount.Grpc.Extensions
                     command.CommandText = @"INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150)";
                     command.ExecuteNonQuery();
 
-                    command.CommandText = @"INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung Galaxy S10', 'Samsung Discount', 100)";
+                    command.CommandText = @"INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100)";
                     command.ExecuteNonQuery();
 
                     logger.LogInformation("Migrated PostegreSQL DataBase");
