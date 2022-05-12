42f51a83929fb4bbcd77fd6356dc2276538cd557
Anderson Masiero <andguto@hotmail.com>
2022-05-12 07:01:24 -0300

    Implementando a camada de infraestrutura. - implementação do envio de e-mail. - adição do EntityFramework e configuração da Entidade Order - implementação das repositorios

---
+++ src/Services/Ordering/Ordering.Infraestructure/Persistence/OrderContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infraestructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders {get;set;}
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        // Deveria ser o nome do usuário, caso o sistema construido tivesse implementação de usuários
+                        entry.Entity.LastModifiedBy = "AndersonMasiero";
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        // Deveria ser o nome do usuário, caso o sistema construido tivesse implementação de usuários
+                        entry.Entity.CreatedBy = "AndersonMasiero";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infraestructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infraestructure.Persistence
+{
+    public class OrderContextSeed
+    {
+
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfigureOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContextSeed).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfigureOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "AndersonMasiero", FirstName ="Anderson", LastName = "Masiero", EmailAddress = "anderson.masiero@gmail.com", Country = "Brasil", TotalPrice = 350};
+            }
+        }
+    }
+}
