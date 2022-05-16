916f7e7c791ef46dc5e7211c17aa21f01eec64c2
Anderson Masiero <andguto@hotmail.com>
2022-05-16 07:32:41 -0300

    Remoção de injeção de dependencia no projeto errado.

--- src/Services/Discount/Discount.API/Startup.cs
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -6,8 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Ordering.Application;
-using Ordering.Infrastructure;
 
 namespace Discount.API
 {
@@ -23,9 +21,6 @@ namespace Discount.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddApplicationServices();
-            services.AddInfrastructureServices(Configuration);
-
             services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
