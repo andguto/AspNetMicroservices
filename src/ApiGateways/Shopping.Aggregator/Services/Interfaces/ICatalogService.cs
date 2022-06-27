266b47c000a086b84b1059094054dd4ec8455c36
Anderson Masiero <anderson.masiero@fitcard.com.br>
2022-06-27 20:24:34 -0300

    Implementando HttpClient Consumer para o Segreggation Gateway API. - HttpClientExtensios - Services Classes - Services Interfaces - Configuração Startup

---
+++ src/ApiGateways/Shopping.Aggregator/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,14 @@
+﻿using Shopping.Aggregator.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<CatalogModel> GetCatalog(string id);
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+
+    }
+}
