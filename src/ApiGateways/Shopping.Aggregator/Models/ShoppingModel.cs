5f44bd9738c7ca103f023bfffa902b7433012d33
Anderson Masiero <anderson.masiero@fitcard.com.br>
2022-06-28 07:41:42 -0300

    Corrigindo Base Address e criando a controller

--- src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -5,7 +5,7 @@ namespace Shopping.Aggregator.Models
     public class ShoppingModel
     {
         public string UserName { get; set; }
-        public BasketModel basketWithProducts { get; set; }
+        public BasketModel BasketWithProducts { get; set; }
         public IEnumerable<OrderResponseModel> Orders { get; set; }
     }
 }
