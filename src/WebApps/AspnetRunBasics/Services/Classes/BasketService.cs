14805d1c33a94d28631420220882b9a9e0f3a6ad
Anderson Masiero <anderson.masiero@fitcard.com.br>
2022-07-18 12:03:58 -0300

    Implementando: -  Models - Services (classes e interfaces) - Startup (injeção de dependencias HTTP) - Metodo de extensão HTTP

---
+++ src/WebApps/AspnetRunBasics/Services/Classes/BasketService.cs
@@ -0,0 +1,41 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Services.Interfaces;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services.Classes
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task ChekoutBasket(BasketCheckoutModel model)
+        {
+            var response = await _client.PostAsJson("/Basket/Checkout", model);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Something a wrong when calling api.");
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+
+        public async Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            var response = await _client.PostAsJson("/Basket", model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<BasketModel>();
+
+            throw new Exception("Something a wrong when calling api.");
+        }
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/Interfaces/ICatalogService.cs
@@ -0,0 +1,14 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<CatalogModel> GetCatalog(string id);
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<CatalogModel> CreateCatalog(CatalogModel model);
+    }
+}
